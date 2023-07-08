9042805a5a8c3fcbb56a7ea6c587c34bcc67d482
Aleksa <007milosevicaleksa@gmail.com>
2023-07-08 23:34:40 +0200

    mama
    mama

--- Assets/Scripts/AirCheck.cs
+++ Assets/Scripts/AirCheck.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class AirCheck : MonoBehaviour
 {
     public bool airCheck;
+    [SerializeField] private Transform airCheckTransform; // Renamed the variable to avoid naming conflicts
     
     // Start is called before the first frame update
     void Start()
